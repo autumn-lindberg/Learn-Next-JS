985cf1d1e5308047eb8dc597a8f217ee876e953b
Autumn <78074803+autumnlindberg1@users.noreply.github.com>
2024-02-05 03:42:26 -0800

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Lato } from 'next/font/google';
+import { Aleo } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const lato = Lato({ weight: '300', subsets: ['latin'] });
+export const aleo = Aleo({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
